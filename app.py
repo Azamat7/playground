815dd9bfe610c0b09204ea681fd490371bf10be8
Azamat Smagulov <s.a.smagulov@gmail.com>
2019-11-28 20:06:32 +0100

    add pymongo

--- app.py
+++ app.py
@@ -4,19 +4,26 @@ from flask import Flask
 from flask import request
 from flask_restful import Api
 from flask_cors import CORS
-
+from pymongo import MongoClient
 
 app = Flask(__name__)
 CORS(app)
 
 api = Api(app)
 
+mongoclient = MongoClient("mongodb://localhost:27017/")
+mongoDB = mongoclient.myappdb
+mongoEntries = mongoDB.collection
+
 
 @app.route("/next", methods=["POST"])
 def next():
     data = request.get_json()
 
+    mongoEntries.insert_one(data)
+
     input_form = data["mainInputValue"]
+
     output = {
         "postOutput": f"output value: {input_form}",
     }
