e41fe3ea37d0b1dea454df88e800fe1eed1a4bbb
Azret Kenzhaliev <bayke@yelp.com>
2020-01-03 23:18:35 +0000

    polishing the code

--- my-app-client/src/index.js
+++ my-app-client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import FlightFrom from './App';
+import Main from './App';
 
-ReactDOM.render(React.createElement(FlightFrom), document.getElementById('root'));
+ReactDOM.render(React.createElement(Main), document.getElementById('root'));
